8ba428cc5e1060040fc8ee38d72131783dd2495a
demim002 <54381560+demim002@users.noreply.github.com>
2019-09-15 10:33:51 -0400

    Update views.py

--- foodie/views.py
+++ foodie/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 #from django.http import HttpResponse
 from .models import posting
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 
@@ -33,3 +34,29 @@ def register(request):
     return render(request,
                  "foodie/register.html",
                  context= {"form":form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out!")
+    return redirect("foodie:homepage")
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "foodie/login.html",
+                    context={"form":form})
