4c7827bfb718cf05a8bbedef036a2a0ed70dc142
demim002 <54381560+demim002@users.noreply.github.com>
2019-09-15 10:33:05 -0400

    Update urls.py

--- foodie/urls.py
+++ foodie/urls.py
@@ -21,5 +21,7 @@ app_name = "foodie"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("register/", views.register, name="register"),
+    path("logout/", views.logout_request, name="logout"),
+    path("login/", views.login_request, name = "login"),
 
     ]
